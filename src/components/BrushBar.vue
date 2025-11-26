d3ae168ffffc7e8b46264e33210d7b582fb88167
Kenton <38125586+Kenton1989@users.noreply.github.com>
2025-11-27 00:08:11 +0800

    add ref blocks into brushes

--- src/components/BrushBar.vue
+++ src/components/BrushBar.vue
@@ -1,16 +1,19 @@
 <template>
-  <div class="brush-bar flex h-12 w-full items-center justify-start gap-4 px-4 py-3">
-    <span
-      v-for="brush in defaultBrushes"
-      :key="brush.name"
-      v-tooltip.top="brush.name"
-      class="aspect-square h-full"
-      :style="brush.style"
-    >
-      <component :is="brush.logoSvg" class="h-full" />
-    </span>
+  <div class="brush-bar flex h-12 w-full items-center justify-start px-2 py-1">
+    <p class="px-2">Brush</p>
+    <BrushBox v-for="brush in defaultBrushes" :key="brush.name" :brushName="brush.name">
+      <component :is="brush.logoSvg" class="h-full w-full transition" :style="brush.style" />
+    </BrushBox>
 
-    <Divider layout="vertical" style="margin: 0" />
+    <Divider layout="vertical" style="margin: 0.5rem" />
+
+    <BrushBox
+      v-for="block in levelStore.levelBlocks"
+      :key="block.blockId"
+      :brushName="`Ref to ${block.name}`"
+    >
+      <BlockCanvas :blockId="block.blockId" class="h-full w-full rounded-xs border" />
+    </BrushBox>
   </div>
 </template>
 <script setup lang="ts">
@@ -21,42 +24,52 @@ import PlayerFloorSvg from '@/assets/player-floor.svg'
 import PlayerSvg from '@/assets/player.svg'
 import SelectSvg from '@/assets/select.svg'
 import WallSvg from '@/assets/wall.svg'
+import { useLevelStore } from '@/stores/level'
 import { Divider } from 'primevue'
-import type { StyleValue } from 'vue'
+import { computed } from 'vue'
+import BlockCanvas from './BlockCanvas.vue'
+import useFocusedBlock from '@/composites/useFocusedBlock'
+import { color } from '@/service/convertors'
+import BrushBox from './BrushBox.vue'
 
-const defaultBrushes: {
-  logoSvg: string
-  name: string
-  style?: StyleValue
-}[] = [
-  {
-    logoSvg: SelectSvg,
-    name: 'Select',
-  },
-  {
-    logoSvg: EraserSvg,
-    name: 'Eraser',
-  },
-  {
-    logoSvg: WallSvg,
-    name: 'Wall',
-    style: 'background-color: lightblue',
-  },
-  {
-    logoSvg: BoxSvg,
-    name: 'Box',
-  },
-  {
-    logoSvg: PlayerSvg,
-    name: 'Player',
-  },
-  {
-    logoSvg: FloorSvg,
-    name: 'Floor',
-  },
-  {
-    logoSvg: PlayerFloorSvg,
-    name: 'Player Floor',
-  },
-]
+const levelStore = useLevelStore()
+const focusedBlock = useFocusedBlock()
+
+const defaultBrushes = computed(() => {
+  const block = focusedBlock.value
+
+  const focusedBlockColor = block ? color.blockToColor(block.color).toString() : 'white'
+
+  return [
+    {
+      logoSvg: SelectSvg,
+      name: 'Select',
+    },
+    {
+      logoSvg: EraserSvg,
+      name: 'Erase',
+    },
+    {
+      logoSvg: WallSvg,
+      name: 'Wall',
+      style: `background-color: ${focusedBlockColor}`,
+    },
+    {
+      logoSvg: BoxSvg,
+      name: 'Box',
+    },
+    {
+      logoSvg: PlayerSvg,
+      name: 'Player',
+    },
+    {
+      logoSvg: FloorSvg,
+      name: 'Floor',
+    },
+    {
+      logoSvg: PlayerFloorSvg,
+      name: 'Player Floor',
+    },
+  ]
+})
 </script>
---
+++ src/components/BrushBox.vue
@@ -0,0 +1,17 @@
+<template>
+  <span
+    v-tooltip.top="brushName"
+    class="hover:bg-emphasis aspect-square h-full p-2"
+    :class="{ 'bg-highlight': selected }"
+  >
+    <span class="max-h-full max-w-full overflow-hidden rounded-xs">
+      <slot></slot>
+    </span>
+  </span>
+</template>
+<script setup lang="ts">
+defineProps<{
+  brushName?: string
+  selected?: boolean
+}>()
+</script>
