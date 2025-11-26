c36e4d88279afeebc1913c03153451286c34ad89
Kenton <38125586+Kenton1989@users.noreply.github.com>
2025-11-26 23:06:38 +0800

    refine main UI width, draft central part of UI

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,11 +1,11 @@
 <template>
   <StartUpDialog></StartUpDialog>
-  <div class="main-page h-screen w-screen overflow-hidden">
+  <div class="main-page h-screen w-screen min-w-240 overflow-hidden">
     <header class="main-header">
       <MainMenuBar />
     </header>
     <div class="main-body flex w-full flex-row gap-1">
-      <aside class="flex max-h-full max-w-80 min-w-40 flex-none flex-col gap-1">
+      <aside class="flex max-h-full w-1/6 min-w-60 flex-none flex-col gap-1">
         <BlockListCard class="w-full flex-1" />
         <InfoCard class="w-full flex-1" title="Block Info">
           <p class="m-0">
@@ -15,20 +15,11 @@
           </p>
         </InfoCard>
       </aside>
-      <main class="h-full grow overflow-auto">
+      <main class="flex h-full min-w-120 grow flex-col items-stretch overflow-auto">
         <BrushBar />
-        <p>{{ JSON.stringify(c) }}</p>
-        <p>remark: HSB is actually HSV</p>
-        <ColorPicker v-model="c" format="hsb" />
-        <p></p>
-        <Button @click="levelStore.clearLevel">Clear</Button>
-        <Button @click="() => (uiStore.setUpDialogVisible = true)">Open</Button>
-        <Button @click="openLevel.open">Faster Open</Button>
-        <h1>Main</h1>
-        <pre> {{ JSON.stringify(levelStore.levelHeader, undefined, 4) }} </pre>
-        <pre> {{ JSON.stringify(levelStore.levelBlocks, undefined, 4) }} </pre>
+        <PaintBoard class="grow" />
       </main>
-      <aside class="flex max-h-full max-w-80 min-w-40 flex-none flex-col gap-1">
+      <aside class="flex max-h-full w-1/6 min-w-60 flex-none flex-col gap-1">
         <InfoCard class="w-full flex-1" title="Cell Info">
           <p class="m-0">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur
@@ -48,20 +39,10 @@
   </div>
 </template>
 <script setup lang="ts">
-import { useOpenLevel } from '@/composites'
 import StartUpDialog from './StartUpDialog.vue'
-import { useLevelStore } from '@/stores/level'
-import { useUiStore } from '@/stores/ui'
-import { Button, ColorPicker } from 'primevue'
 import MainMenuBar from './MainMenuBar.vue'
 import InfoCard from './InfoCard.vue'
 import BrushBar from './BrushBar.vue'
 import BlockListCard from './BlockListCard.vue'
-import { ref } from 'vue'
-
-const levelStore = useLevelStore()
-const uiStore = useUiStore()
-const openLevel = useOpenLevel()
-
-const c = ref({ h: 20, s: 100, b: 100 })
+import PaintBoard from './PaintBoard.vue'
 </script>
---
+++ src/components/PaintBoard.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex items-center justify-center p-10">
+    <BlockCanvas :blockId="uiStore.focusedBlockId" class="h-full" />
+  </div>
+</template>
+<script setup lang="ts">
+import { useUiStore } from '@/stores/ui'
+import BlockCanvas from './BlockCanvas.vue'
+
+const uiStore = useUiStore()
+</script>
