a93169591fc82f5d80a9f27f2090d15c70cdeb3f
Kenton <38125586+Kenton1989@users.noreply.github.com>
2025-11-26 21:08:46 +0800

    draft block list item

--- src/components/BlockListItem.vue
+++ src/components/BlockListItem.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="block-list-item flex min-h-20 justify-start align-middle">
-    <canvas ref="preview-ref" class="size-15 rounded-xs"></canvas>
-    <p>
-      {{ props.block.name }}
-    </p>
+  <div class="block-list-item flex min-h-20 items-center justify-start gap-4 overflow-auto">
+    <canvas ref="preview-ref" class="size-15 rounded-sm"></canvas>
+    <p class="grow">{{ props.block.name }}</p>
+    <Button icon="pi pi-trash" size="small" />
   </div>
 </template>
 <script setup lang="ts">
 import { useBlockPreview } from '@/composites'
 import type { LevelBlock } from '@/models/level'
+import { Button } from 'primevue'
 import { useTemplateRef } from 'vue'
 
 const props = defineProps<{
@@ -16,6 +16,5 @@ const props = defineProps<{
 }>()
 
 const previewRef = useTemplateRef<HTMLCanvasElement>('preview-ref')
-
 useBlockPreview(previewRef, props.block.blockId)
 </script>
