f7600f60f0dfbdad42416be8cd55f51d7c9d7b31
Kenton <38125586+Kenton1989@users.noreply.github.com>
2025-11-30 08:41:28 +0800

    double click a ref to focus the block

--- src/components/BlockCell.vue
+++ src/components/BlockCell.vue
@@ -1,21 +1,25 @@
 <template>
   <div
-    ref="cell-ref"
-    class="relative border-2"
-    :style="{ borderColor: cellBorderColor }"
-    @click="selectCell"
+    class="outline-primary relative border-2"
+    :class="{
+      'outline-4': isFocused,
+    }"
+    :style="{ borderColor: cellBorderColor, zIndex: isFocused ? 10 : 0 }"
   >
     <LevelObjectCanvas
       v-if="objOnTheFloor"
-      :style="{ zIndex: baseZIndex + 1, textAlign: 'right' }"
-      class="absolute top-0 right-0 bottom-0 left-0 h-full w-full"
+      :style="{ zIndex: 1, textAlign: 'right' }"
+      class="absolute top-0 right-0 bottom-0 left-0 h-full w-full cursor-pointer"
       :object="objOnTheFloor"
       :parentColor="parentColor"
+      @click="selectCell"
+      @dblclick="doubleClickCell"
     />
     <FloorCanvas
       v-if="floor"
-      class="absolute top-0 right-0 bottom-0 left-0 h-full w-full"
+      class="absolute top-0 right-0 bottom-0 left-0 h-full w-full cursor-pointer"
       :object="floor"
+      @click="selectCell"
     />
     <div v-else class="absolute top-0 right-0 bottom-0 left-0 h-full w-full"></div>
   </div>
@@ -23,20 +27,18 @@
 <script setup lang="ts">
 import type { BlockCell, BlockColor } from '@/models/level'
 import type { Immutable } from '@/models/utils'
-import { computed, useTemplateRef } from 'vue'
+import { computed } from 'vue'
 import LevelObjectCanvas, { FloorCanvas } from './level-object/canvas'
 import { useUiStore } from '@/stores/ui'
 
 const props = defineProps<{ cell: Immutable<BlockCell>; parentColor?: BlockColor }>()
 
-const cellRef = useTemplateRef('cell-ref')
-
-const baseZIndex = computed(() =>
-  cellRef.value?.style?.zIndex ? Number(cellRef.value?.style?.zIndex) : 0,
-)
-
 const floor = computed(() => props.cell.objects.find((o) => o.type === 'Floor'))
 const objOnTheFloor = computed(() => props.cell.objects.find((o) => o.type !== 'Floor'))
+
+const isFocused = computed(
+  () => uiStore.focusedCellInfo?.x === props.cell.x && uiStore.focusedCellInfo.y === props.cell.y,
+)
 
 const cellBorderColor = computed(() => (objOnTheFloor.value && floor.value ? 'white' : 'black'))
 
@@ -48,5 +50,15 @@ const selectCell = () => {
     y: props.cell.y,
     objects: [objOnTheFloor.value, floor.value].filter((o) => o !== undefined),
   }
+}
+
+const doubleClickCell = () => {
+  if (!objOnTheFloor.value || objOnTheFloor.value.type !== 'Ref') {
+    return
+  }
+  const refObj = objOnTheFloor.value
+  if (refObj.parentId === refObj.referToBlockId) return
+
+  uiStore.focusedBlockId = refObj.referToBlockId
 }
 </script>
