0a2e53c21de6a3311c1b649f2957a5764cc8d7df
Kenton <38125586+Kenton1989@users.noreply.github.com>
2025-11-30 08:23:26 +0800

    add wall info card

--- src/components/level-object/info-card/WallInfoCard.vue
+++ src/components/level-object/info-card/WallInfoCard.vue
@@ -1,11 +1,25 @@
 <template>
   <InfoCard title="Wall Info">
-    <pre>{{ JSON.stringify(props.object, null, 4) }}</pre>
+    <div class="flex flex-col gap-2">
+      <InfoLine label="Player?">
+        <PlayerSettingInput
+          class="w-full"
+          :modelValue="object.playerSetting"
+          @update:modelValue="(playerSetting) => update({ playerSetting })"
+        />
+      </InfoLine>
+      <SecondaryTags :object="object" />
+    </div>
   </InfoCard>
 </template>
 <script setup lang="ts">
 import type { LevelWall } from '@/models/level'
-import { InfoCard } from '@/components/templates'
+import { InfoCard, InfoLine } from '@/components/templates'
+import { PlayerSettingInput } from '@/components/inputs'
+import SecondaryTags from './SecondaryTags.vue'
+import useEditLevelObject from '@/composites/useEditLevelObject'
 
 const props = defineProps<{ object: LevelWall }>()
+
+const { update } = useEditLevelObject(props.object)
 </script>
